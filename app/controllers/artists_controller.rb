4bcca19eaa6a80820175ff4a565209be81fc6595
Roshanak Agahi <rosh.agahi@gmail.com>
2021-02-17 06:21:33 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,8 +1,10 @@
 class ArtistsController < ApplicationController
   def index
+    @artists = Artist.all
   end
 
   def show
+    @artist = Artist.find(params[:id])
   end
 
   def new
@@ -47,4 +49,8 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def set_artist
+    @artist = Artist.find(params[:id])
+  end 
 end
